5c743d65bedd5d63297045c64e313d8549934cd3
TAMIM <saifur.tamim@gmail.com>
2021-08-17 00:09:27 +0600

    minified js file

--- bank.js
+++ bank.js
@@ -1,5 +1,5 @@
 const depositeAmount = document.getElementById('depositeAmount');
-const withdrawAmoount = document.getElementById('withdrawAmount');
+const withdrawAmount = document.getElementById('withdrawAmount');
 const balanceAmount = document.getElementById('balanceAmount');
 
 const depositeInput = document.getElementById('depositeInput');
@@ -7,35 +7,15 @@ const withdrawInput = document.getElementById('withdrawInput');
 const depositeBtn = document.getElementById('depositeButton');
 const withdrawBtn = document.getElementById('withdrawButton');
 
+function moneyOperation(getInput, throwOutput) {
+    throwOutput.innerText = parseFloat(throwOutput.innerText) + Math.abs(parseFloat(getInput));// change deposite amount or withdraw amount
 
-function setAmounts(deposite, withdraw, balance) {
-    // set deposite value to depositeAmount on html page
-    depositeAmount.innerText = deposite;
-    // set deposite value to withdrawAmount on html page
-    withdrawAmount.innerText = withdraw;
-    // set deposite value to balanceAmount on html page
-    balanceAmount.innerText = balance;
+    //update balance
+    balanceAmount.innerText = parseFloat(balanceAmount.innerText) + parseFloat(getInput);
+
+    input = ''; // clear the input field
 }
 
-setAmounts(0, 0, 2536);
+depositeBtn.addEventListener('click', function () { if (depositeInput.value > 0) { moneyOperation(depositeInput.value, depositeAmount) } })
 
-
-depositeBtn.addEventListener('click', function () {
-    const newDeposite = parseFloat(depositeInput.value);
-
-    depositeAmount.innerText = parseFloat(depositeAmount.innerText) + newDeposite;
-    balanceAmount.innerText = parseFloat(balanceAmount.innerText) + newDeposite;
-
-    // clear the input field
-    depositeInput.value = '';
-})
-
-withdrawBtn.addEventListener('click', function () {
-    const newWithdraw = parseFloat(withdrawInput.value);
-
-    withdrawAmoount.innerText = parseFloat(withdrawAmoount.innerText) + newWithdraw;
-    balanceAmount.innerText = parseFloat(balanceAmount.innerText) - newWithdraw;
-
-    // clear the input field
-    withdrawInput.value = '';
-})
+withdrawBtn.addEventListener('click', function () { if (withdrawInput.value > 0) { moneyOperation(-withdrawInput.value, withdrawAmount) } })
